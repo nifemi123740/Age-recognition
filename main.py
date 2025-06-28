2eb3a82e534ab70402bff5281063980a077bad0c
nifemi123740 <deborahadegbite407@gmail.com>
2025-06-28 20:51:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hi i am Age recognitioner\n Please write your age below")
+#input statement
+your_name=input("Enter your name: ")
+your_year=input("Enter your birthday year: ")
+your_month=input("Enter your birthday month: ")
+your_day=input("Enter your birthday day: ")
+#output statement
+print("Your name is:",your_name)
+print("Your birthday is:",your_day,your_month,your_year)
